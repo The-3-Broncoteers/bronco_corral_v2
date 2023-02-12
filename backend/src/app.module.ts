ea777c76b25b8b4e589db40e960a699713b4eb43
dtchitt <danielchittenden@yahoo.com>
2023-02-12 15:36:30 -0800

    daniel hw3 commit

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DanielController } from './hw-three/daniel.controller';
 
 @Module({
   imports: [],
+  controllers: [AppController, DanielController],
+  providers: [AppService],
 })
 export class AppModule {}
