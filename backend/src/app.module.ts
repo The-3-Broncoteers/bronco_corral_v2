6dca6f6e1f7375217734986e2fe4c46739a49851
Maestroarm <artakvanyan@gmail.com>
2023-02-23 15:21:54 -0800

    artak hw3

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ArtakController } from './hw-three/artak.controller';
 import { DanielController } from './hw-three/daniel.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, DanielController],
+  controllers: [AppController, DanielController, ArtakController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
