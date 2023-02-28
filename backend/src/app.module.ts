3e317b527d9bc4162529ee3479a8b2dcf6ea06d3
Maestroarm <artakvanyan@gmail.com>
2023-02-28 00:06:41 -0800

    add login

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArtakController } from './hw-three/artak.controller';
 import { DanielController } from './hw-three/daniel.controller';
+import { LoginController } from './login.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, DanielController, ArtakController],
+  controllers: [AppController, DanielController, ArtakController, LoginController],
   providers: [AppService],
 })
 export class AppModule { }
