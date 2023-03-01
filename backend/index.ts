71be88b5a6291f8b0a878458b36244a9357ba929
dtchitt <danielchittenden@yahoo.com>
2023-02-28 22:51:25 -0800

    change backend to express

---
+++ backend/index.ts
@@ -0,0 +1,46 @@
+import { PrismaClient } from '@prisma/client';
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json);
+const port: number = 3001;
+
+app.listen(port, () => {
+	console.log(`Express is listening at http://localhost:${port}`);
+
+	const prisma = new PrismaClient({ log: ['query'] });
+	async function main() {
+		const users = await prisma.users.findMany();
+		console.log(users);
+	}
+
+	try {
+		main()
+			.then(async () => {
+				await prisma.$disconnect();
+			})
+			.catch(async (e) => {
+				console.error(e);
+				await prisma.$disconnect();
+				process.exit(1);
+			});
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+app.post('/create', async (req, res) => {
+	const email = req.body.email;
+	const password = req.body.password;
+
+	const prisma = new PrismaClient({ log: ['query'] });
+
+	const newUser = await prisma.users.create({
+		data: {
+			email: email,
+			password: password,
+		},
+	});
+});
--- frontend/src/components/FormInput.tsx
+++ frontend/src/components/FormInput.tsx
@@ -1,33 +1,51 @@
+import Axios from 'axios';
 import { useState } from 'react';
+import { Container } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 export const FormInput = () => {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
 
-	const handleSubmit = (e: { preventDefault: () => void }) => {
-		e.preventDefault();
-		const data = { email, password };
-		fetch('/api/login', {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify(data),
-		})
-			.then((response) => response.json())
-			.then((data) => console.log(data))
-			.catch((error) => console.error(error));
+	const handleSubmit = () => {
+		console.log(`email: ${email} pw: ${password}`);
+		Axios.post('http://localhost:3001/create', {
+			email: email,
+			password: password,
+		}).then(() => {
+			console.log('submited');
+		});
 	};
 
 	return (
-		<form onSubmit={handleSubmit}>
-			<label>
-				Email:
-				<input type='email' value={email} onChange={(e) => setEmail(e.target.value)} />
-			</label>
-			<label>
-				Password:
-				<input type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
-			</label>
-			<button type='submit'>Submit</button>
-		</form>
+		<Container fluid className='w-25 pt-5'>
+			<Form onSubmit={handleSubmit}>
+				<Form.Group className='mb-3' controlId='formBasicEmail'>
+					<Form.Label>Email address</Form.Label>
+					<Form.Control
+						type='text'
+						placeholder='Enter email'
+						onChange={(event) => {
+							setEmail(event.target.value);
+						}}
+					/>
+				</Form.Group>
+
+				<Form.Group className='mb-3' controlId='formBasicPassword'>
+					<Form.Label>Password</Form.Label>
+					<Form.Control
+						type='password'
+						placeholder='Enter password'
+						onChange={(event) => {
+							setPassword(event.target.value);
+						}}
+					/>
+				</Form.Group>
+				<Button variant='primary' type='submit'>
+					Submit
+				</Button>
+			</Form>
+		</Container>
 	);
 };
