5ea856677fb50f11483400cdeea6663bfbfdf1bb
Maestroarm <artakvanyan@gmail.com>
2023-03-04 23:35:32 -0800

    set up Jest and added basic unit test

---
+++ backend/index.test.ts
@@ -0,0 +1,20 @@
+//unit test for endpoint with sample email and password
+
+const request = require('supertest');
+const express = require('express');
+const app = express();
+
+describe('POST /api/create', () => {
+  it('Responds with 200 status and success: true', async () => {
+    const response = await request(app)
+      .post('/api/create')
+      .send({
+        email: 'test@example.com',
+        password: 'password123'
+      });
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual({ success: true });
+  });
+});
+
+//do "jest index.test.ts" to run the test
