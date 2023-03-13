afe9615c6d99178ba8e570691dd7b62c75e97e66
dtchitt <danielchittenden@yahoo.com>
2023-03-13 15:29:58 -0700

    clean up code, remove formInput.tsx

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,6 +1,4 @@
-import { FormInput } from '../components/FormInput';
 import { LoginForm } from '../components/LoginForm';
-import { SignupForm } from '../components/SignupForm';
 
 export function Login() {
 	return <LoginForm></LoginForm>;
