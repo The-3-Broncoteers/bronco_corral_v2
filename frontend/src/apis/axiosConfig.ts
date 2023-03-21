318b115f3cfeba4bd69b42dada144c5795fc38ee
dtchitt <danielchittenden@yahoo.com>
2023-03-20 19:39:20 -0700

    update base url

--- frontend/src/apis/axiosConfig.ts
+++ frontend/src/apis/axiosConfig.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL: string = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3001/api';
+const baseURL: string = process.env.NODE_ENV === 'production' ? 'api' : 'http://localhost:3001/api';
 
 export default axios.create({
 	baseURL: baseURL,
