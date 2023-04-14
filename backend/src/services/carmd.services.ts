36f37fd1435f21ae6ad3aa943afb9f5f2bc428da
dtchitt <danielchittenden@yahoo.com>
2023-04-13 21:42:18 -0700

    add axios and update module to import

--- backend/src/services/carmd.services.ts
+++ backend/src/services/carmd.services.ts
@@ -1,16 +1,17 @@
-const axios = require('axios');
+import axios from 'axios';
 
 const authKey = '';
 const token = '';
 
 export const getMaintenances = async (vin: number, mileage: number) => {
-    const apiUrl = `https://api.carmd.com/v3.0/maint?vin=${vin}&mileage=${mileage}`;
-    const response = await axios.get(apiUrl, {
-        headers: {
-            'content-type': 'application/json',
-            'authorization': authKey,
-            'partner-token': token
-        }
-    });
-    return await response.data;
-}
+	const apiUrl = `https://api.carmd.com/v3.0/maint?vin=${vin}&mileage=${mileage}`;
+	const response = await axios.get(apiUrl, {
+		headers: {
+			'content-type': 'application/json',
+			authorization: authKey,
+			'partner-token': token,
+		},
+	});
+
+	return await response.data;
+};
