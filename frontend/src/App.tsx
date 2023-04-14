588a4bba5b907858016c2871384924cabc42d563
dtchitt <danielchittenden@yahoo.com>
2023-04-13 21:44:21 -0700

    update frontend routing & add logout component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,18 +3,16 @@ import { UserDashBoard } from './pages/UserDashBoard';
 import { MainNav } from './components/MainNav';
 import { Login } from './pages/Login';
 import { LandingPage } from './pages/LandingPage';
+import { Logout } from './pages/Logout';
 
 function App() {
 	return (
 		<Routes>
 			<Route path='/' element={<LandingPage />} />
-			<Route path='/' element={<MainNav />}>
-				<Route path='login' element={<Login />} />
-				<Route path='home' element={<UserDashBoard />} />
-				<Route path='user' element={<MainNav />}>
-					<Route path='dashboard' element={<UserDashBoard />} />
-					<Route path='logout' element={<Login />} />
-				</Route>
+			<Route path='/login' element={<Login />} />
+			<Route path='/user/:userId/*' element={<MainNav />}>
+				<Route path='dashboard' element={<UserDashBoard />} />
+				<Route path='logout' element={<Logout />} />
 			</Route>
 		</Routes>
 	);
