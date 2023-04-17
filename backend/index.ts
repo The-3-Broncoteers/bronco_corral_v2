580a6dafd0ffd4d51e4a6f9681584811f134331a
dtchitt <danielchittenden@yahoo.com>
2023-04-17 13:05:13 -0700

    update packages and add log files to gitignore

--- backend/index.ts
+++ backend/index.ts
@@ -2,25 +2,29 @@ import { PrismaClient } from '@prisma/client';
 import { userRouter } from './src/routes/user.routes';
 import { authRouter } from './src/routes/user.auth.routes';
 import { carmdRouter } from './src/routes/carmd.routes';
+import { errorHandler } from './src/middleware/errorHandler';
+import { eventLogger } from './src/middleware/eventLogger';
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { prisma } from './prisma/prisma';
 
 dotenv.config();
 
 const app = express();
-const port: number = 3001;
+const PORT: string | number = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(eventLogger);
+app.use(cors()); //Can use corsOptions for safety. (Prevent unauthorized people from making api requests)
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use('/api/users', userRouter);
-app.use('/api/auth', authRouter);
-app.use('/api/maintenance', carmdRouter);
+app.use('/users', userRouter);
+app.use('/auth', authRouter);
+app.use('/maintenance', carmdRouter);
+app.use(errorHandler);
 
-const prisma = new PrismaClient({ log: ['query'] });
-
-app.listen(port, async () => {
-	console.log(`Express is listening at http://localhost:${port}\nTesting Prisma Connection...`);
+app.listen(PORT, async () => {
+	console.log(`Express is listening on port ${PORT}\nTesting Prisma Connection...`);
 
 	await prisma
 		.$connect()
