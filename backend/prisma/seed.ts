da445385bc713344450ae7adc1fa77501d8a1be0
dtchitt <danielchittenden@yahoo.com>
2023-04-17 13:05:27 -0700

    update prisma modal

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,13 +1,12 @@
 import { PrismaClient } from '@prisma/client';
+import { prisma } from './prisma';
 
 /**
  * This will check and populate (if its not found) the database tables against the models in the main function
  * cli: npx prisma db seed
  */
-const prisma = new PrismaClient();
-
 async function main() {
-	const newUser = await prisma.users.create({
+	const newUser = await prisma.user.create({
 		data: {
 			email: 'danielTest',
 			password: 'one',
