457aaadcf7579ae218c6a5d5073fcb18f3e1e21e
dtchitt <danielchittenden@yahoo.com>
2023-04-19 11:29:33 -0700

    add all new and updated routes to index.ts

--- backend/index.ts
+++ backend/index.ts
@@ -1,13 +1,17 @@
-import { PrismaClient } from '@prisma/client';
+import { prisma } from './prisma/prisma';
 import { userRouter } from './src/routes/user.routes';
-import { authRouter } from './src/routes/user.auth.routes';
 import { carmdRouter } from './src/routes/carmd.routes';
 import { errorHandler } from './src/middleware/errorHandler';
 import { eventLogger } from './src/middleware/eventLogger';
+import { authRouter } from './src/routes/auth.routes';
+import { registerRouter } from './src/routes/register.routes';
+import { verifyJWT } from './src/middleware/verifyJWT';
+import { logoutRouter } from './src/routes/logout.routes';
+import { refreshRouter } from './src/routes/refresh.routes';
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
-import { prisma } from './prisma/prisma';
 
 dotenv.config();
 
@@ -15,11 +19,16 @@ const app = express();
 const PORT: string | number = process.env.PORT || 3001;
 
 app.use(eventLogger);
-app.use(cors()); //Can use corsOptions for safety. (Prevent unauthorized people from making api requests)
+app.use(cors()); //Can use cors options for safety. (Prevent unauthorized people from making api requests)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use('/users', userRouter);
+app.use(cookieParser());
+app.use('/register', registerRouter);
 app.use('/auth', authRouter);
+app.use('/refresh', refreshRouter);
+app.use('/logout', logoutRouter);
+app.use(verifyJWT);
+app.use('/users', userRouter);
 app.use('/maintenance', carmdRouter);
 app.use(errorHandler);
 
---
+++ backend/src/services/logout.services.ts
@@ -0,0 +1,21 @@
+import { User } from '@prisma/client';
+import { Http409Error } from '../utils/httpErrors/errors/Http409Error';
+import { Http500Error } from '../utils/httpErrors/errors/Http500Error';
+import { prisma } from '../../prisma/prisma';
+
+export const logoutUser = async (token: string) => {
+	try {
+		//TODO check if user has token(s)
+
+		return;
+	} catch (error) {
+		if (
+			error instanceof Error &&
+			error.message.includes('Unique constraint failed on the fields: (`email`)')
+		) {
+			throw new Http409Error('Email already exists');
+		} else {
+			throw new Http500Error('Error occurred during password hashing');
+		}
+	}
+};
