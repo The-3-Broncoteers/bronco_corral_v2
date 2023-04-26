39f16fcb2e44fd41696b5286fd6dc91fc6132a45
owwix <53359125+owwix@users.noreply.github.com>
2023-04-25 21:29:36 -0700

    create vehicle & get vehicle route almost finished

--- backend/src/controllers/carprofile.controller.ts
+++ backend/src/controllers/carprofile.controller.ts
@@ -6,7 +6,7 @@ import { HttpStatus } from '../utils/httpErrors/HttpStatus';
 export const createVehicle = async (req: Request, res: Response): Promise<void> => {
 	try {
 		console.log(req.body.VIN);
-		await vehicleCreater(req.body.VIN);
+		await vehicleCreater(req.body.VIN, req.body.user);
 		res.sendStatus(200);
 	} catch (error) {
 		res.status(404).json({ message: (error as HttpStatus).message });
--- backend/src/services/carprofile.services.ts
+++ backend/src/services/carprofile.services.ts
@@ -3,32 +3,45 @@ import { env } from 'process';
 import { prisma } from '../../prisma/prisma';
 import { getMaintenances } from './carmd.services';
 
-export const vehicleCreater = async (vin: string) => {
+export const vehicleCreater = async (vin: string, user: string) => {
 	const url = `http://api.carmd.com/v3.0/decode?vin=${vin}`;
-	// const res = await axios.get(url, {
-	// 	headers: {
-	// 		'content-type': 'application/json',
-	// 		authorization: env.CARMD_TOKEN_SECRET1,
-	// 		'partner-token': env.CARMD_PARTNER_TOKEN_SECRET1,
-	// 	},
-	// });
+	const res = await axios.get(url, {
+		headers: {
+			'content-type': 'application/json',
+			authorization: env.CARMD_TOKEN_SECRET1,
+			'partner-token': env.CARMD_PARTNER_TOKEN_SECRET1,
+		},
+	});
 
-	// const year = res.data.data.year;
-	// const make = res.data.data.make;
-	// const model = res.data.data.model;
+	const year = res.data.data.year;
+	const make = res.data.data.make;
+	const model = res.data.data.model;
 
-	// // another api call to get maintenances?
-	// const maintenances: string[] = [];
+	// should be from decoded token
+	const userEmail: string = 'tester@test.com';
 
-	// // await prisma.vehicle.create({
-	// // 	data: {
-	// // 		vin: vin,
-	// // 		year: year,
-	// // 		make: make,
-	// // 		model: model,
-	// // 		maintenances: maintenances,
-	// // 	},
-	// // });
+	await prisma.vehicle.create({
+		data: {
+			vin: vin,
+			year: year,
+			make: make,
+			model: model,
+			userEmail: userEmail,
+		},
+	});
 };
 
-export const fetchVehicles = async (token: string) => {};
+export const fetchVehicles = async (user: string) => {
+	// should be from decoded token
+	const userEmail: string = 'tester@test.com';
+
+	const vehicles = await prisma.vehicle.findMany({
+		where: {
+			userEmail: userEmail,
+		},
+	});
+
+	// how do i
+	console.log(vehicles);
+	return vehicles;
+};
