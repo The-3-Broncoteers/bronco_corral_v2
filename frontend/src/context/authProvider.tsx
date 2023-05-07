53b77603466b11c38f3548e73e779e5826ea5beb
Maestroarm <artakvanyan@gmail.com>
2023-05-06 23:12:54 -0700

    added persistent user login

--- frontend/src/context/authProvider.tsx
+++ frontend/src/context/authProvider.tsx
@@ -2,19 +2,32 @@ import { createContext, useState } from 'react';
 
 interface AuthContextType {
 	auth: any;
+	persist: boolean;
 	setAuth: React.Dispatch<React.SetStateAction<any>>;
+	setPersist: React.Dispatch<React.SetStateAction<any>>;
 }
 
 const AuthContext = createContext<AuthContextType>({
 	auth: {},
+	persist: false,
 	// eslint-disable-next-line @typescript-eslint/no-empty-function
 	setAuth: () => {},
+	// eslint-disable-next-line @typescript-eslint/no-empty-function
+	setPersist: () => {},
 });
 
 export const AuthProvider = ({ children }: any) => {
 	const [auth, setAuth] = useState({});
+	const [persist, setPersist] = useState(() => {
+		const persistValue = localStorage.getItem('persist');
+		return persistValue ? JSON.parse(persistValue) : false;
+	});
 
-	return <AuthContext.Provider value={{ auth, setAuth }}>{children}</AuthContext.Provider>;
+	return (
+		<AuthContext.Provider value={{ auth, setAuth, persist, setPersist }}>
+			{children}
+		</AuthContext.Provider>
+	);
 };
 
 export default AuthContext;
